eb9d405fb2fd75e187e54fd794ee3964eec31af8
Utku Ufuk <utkuufuk@gmail.com>
2022-04-16 23:19:21 +0300

    add unit tests

--- README.md
+++ README.md
@@ -1,1 +1,7 @@
 # io-ts-types-ext
+Extension types and codecs for [`io-ts-types`](https://github.com/gcanti/io-ts-types)
+
+## Development
+- Build: `yarn`
+- Lint: `yarn lint`
+- Test: `yarn test`
---
+++ src/codecs/moment.test.ts
@@ -0,0 +1,62 @@
+import * as E from 'fp-ts/Either'
+import { pipe } from 'fp-ts/lib/function'
+import moment from 'moment-timezone'
+
+import { MomentFromString } from './moment'
+
+describe('moment', () => {
+  const TEST_MOMENT_STR = '2022-12-22:01:23:45.000Z'
+
+  describe('MomentFromString', () => {
+    it('encodes', () => {
+      const encoded = MomentFromString.encode(moment(TEST_MOMENT_STR))
+      expect(encoded).toEqual(TEST_MOMENT_STR)
+    })
+
+    it('decodes valid moment from string', () => {
+      pipe(
+        TEST_MOMENT_STR,
+        MomentFromString.decode,
+        E.fold(
+          err => {
+            throw new Error(`Unexpected error: ${err}`)
+          },
+          m => {
+            expect(m.isSame(moment(TEST_MOMENT_STR))).toBe(true)
+          },
+        ),
+      )
+    })
+
+    it('returns error when decoding valid moment object instead of its string representation', done => {
+      pipe(
+        moment(TEST_MOMENT_STR),
+        MomentFromString.decode,
+        E.fold(
+          _ => {
+            done()
+          },
+          _ => {
+            throw new Error('Decode should have failed!')
+          },
+        ),
+      )
+    })
+
+    it('returns error when decoding invalid moment from string', done => {
+      const str = '20221222012345000'
+      pipe(
+        str,
+        MomentFromString.decode,
+        E.fold(
+          _ => {
+            done()
+          },
+          _ => {
+            throw new Error('Decode should have failed!')
+          },
+        ),
+      )
+    })
+  })
+})
